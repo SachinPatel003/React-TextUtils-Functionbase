07c6163f1f4de8fe23bd220db73fd43eec799400
Sachin <sachinpatel460@gmail.com>
2022-08-20 21:49:33 +0530

    cheat sheet folder commit

---
+++ Cheat Sheet/Cheat Sheet Fetch API.js
@@ -0,0 +1,36 @@
+// GET request 
+
+url = "https://reqres.in/api/users?page=2"
+
+fetch(url)
+.then((res)=>{
+    // return res.text()  //if you need data in simple text
+    return res.json() //if you need (parse) data in Object
+})
+.then((data)=>{
+    console.log(data); 
+})
+
+// POST Request 
+
+url = "https://reqres.in/api/users?page=2"
+data1 = {"name": "morpheus","job": "leader"}
+params = {
+    method: 'post',
+    // body: data1    // if your data1 is already in string
+    body: JSON.stringify(data1), //if you data1 is not a string but object then use this because body need data in string form
+    headers: {
+        'Content-Type':'application/json' //Content type is change as per data
+    }
+}
+
+fetch(url, params)
+.then((res)=>{
+    // return res.text(); //if you need data in simple text
+    return res.json(); //if you need (parse) data in Object
+})
+.then((data)=>{
+    console.log(data);
+    
+})
+
